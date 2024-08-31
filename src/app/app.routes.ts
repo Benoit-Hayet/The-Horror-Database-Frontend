6dcc1cba85616eb975b90af2cb4a5e6cb648bb9f
Benoit-Hayet <hayetbenoit@gmail.com>
2024-08-31 20:22:42 +0200

    navigation ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DatabaseComponent } from './database/database.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    {path:'database',
+    component: DatabaseComponent},
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    {
+        path:'about',
+        component:AboutComponent
+    }
+];

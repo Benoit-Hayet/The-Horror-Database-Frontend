45f3deac197c68a41c99c238ab453875c9fb901d
Benoit-Hayet <hayetbenoit@gmail.com>
2024-09-22 11:37:32 +0200

    Card Creation 1st Step

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { movieCards } from '../model/movieCards.model';
+import { movies } from '../movies/movie.data';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  movieCards : movieCards[] = movies;
 }
---
+++ src/app/model/movieCards.model.ts
@@ -0,0 +1,13 @@
+export interface movieCards {
+    id: number;
+    title: string;
+    country: string;
+    releaseYear: number;
+    director: string;
+    synopsis: string;
+    status: string;
+    posterUrl: string;
+    createdBy: number;
+    createdAt: string;
+    genreName: string[];
+}

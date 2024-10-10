6d87f4078be898b63dad333e75b39625d207355c
Benoit-Hayet <hayetbenoit@gmail.com>
2024-10-10 12:17:29 +0200

    sidebar Ok

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { country } from '../model/country.model';
 import { countries } from '../data/country.data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })

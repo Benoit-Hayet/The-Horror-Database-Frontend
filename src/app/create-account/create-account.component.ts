81e485bc9180875fc16e37477e6a85d5a38aa18e
Benoit-Hayet <hayetbenoit@gmail.com>
2024-10-15 14:06:56 +0200

    Create account ok

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss'
 })
 export class CreateAccountComponent {
+  formBuilder = inject(FormBuilder);
 
-}
+  registerForm = this.formBuilder.group ({
+    name:[''],
+    lastname:[''],
+    dateOfBirth:[''],
+    email:[''],
+    password:[''],
+  })
+  onSubmit() {
+    console.log(this.registerForm.value)
+  }
+  }
+

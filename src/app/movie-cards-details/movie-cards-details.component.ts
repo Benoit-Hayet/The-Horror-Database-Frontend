3497361ff8d020c98914fb63a0b36b3eca161da5
Benoit-Hayet <hayetbenoit@gmail.com>
2024-11-01 18:28:50 +0100

    Json Server Api

--- src/app/movie-cards-details/movie-cards-details.component.ts
+++ src/app/movie-cards-details/movie-cards-details.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { movieCards } from '../model/movieCards.model';
-import { movies } from '../data/movie.data';
 import { CommonModule } from '@angular/common';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-movie-cards-details',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './movie-cards-details.component.html',
-  styleUrl: './movie-cards-details.component.scss'
+  styleUrl: './movie-cards-details.component.scss',
 })
 export class MovieCardsDetailsComponent {
+  movieCards: movieCards[] = [];
 
-    movieCards : movieCards[] = movies;
-  
-    get sortedMoviesById() : movieCards [] {
-      return this.movieCards.sort((a, b) => b.id - a.id).slice(0, 15);
-  }}
-  
+  constructor(private apiService: ApiService) {}
 
+  ngOnInit() {
+    this.apiService.getMovies().subscribe((response) => {
+      this.movieCards = response;
+    });
+  }
+
+  get sortedMoviesById(): movieCards[] {
+    return this.movieCards.sort((a, b) => b.id - a.id).slice(0, 15);
+  }
+}
--- src/app/movie-cards/movie-cards.component.ts
+++ src/app/movie-cards/movie-cards.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { movieCards } from '../model/movieCards.model';
-import { movies } from '../data/movie.data';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-movie-cards',
@@ -11,7 +11,15 @@ import { movies } from '../data/movie.data';
   styleUrl: './movie-cards.component.scss'
 })
 export class MovieCardsComponent {
-  movieCards : movieCards[] = movies;
+  movieCards : movieCards[] =  [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getMovies().subscribe((response) => {
+      this.movieCards = response;
+    });
+  }
 
   get sortedMoviesById() : movieCards [] {
     return this.movieCards.sort((a, b) => b.id - a.id).slice(0, 15);

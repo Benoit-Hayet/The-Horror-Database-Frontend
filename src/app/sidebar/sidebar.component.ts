e6824dd8ce31cf80a66b40802800a0bcf4b5a9f9
Benoit-Hayet <hayetbenoit@gmail.com>
2024-11-04 16:02:58 +0100

    Add movie component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { country } from '../model/country.model';
 import { countries } from '../data/country.data';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })

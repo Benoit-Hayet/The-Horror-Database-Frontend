51748baa3aff61b763c2d33cc8db8e53d8a6a4c2
Benoit-Hayet <hayetbenoit@gmail.com>
2024-11-19 21:02:06 +0100

    Add movie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,57 +10,57 @@ import { FavoriteMovieComponent } from './favorite-movie/favorite-movie.componen
 import { MyReviewComponent } from './my-review/my-review.component';
 import { MyAddMovieComponent } from './my-add-movie/my-add-movie.component';
 import { MovieReviewComponent } from './movie-review/movie-review.component';
-
-
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:HomeComponent
-    },
-    {
-        path:'home',
-        component:HomeComponent
-    },
-    {path:'database',
-    component: DatabaseComponent},
-    {
-        path:'login',
-        component:LoginComponent
-    },
-    {
-        path:'about',
-        component:AboutComponent
-    },
-    {
-        path:'create-account',
-        component:CreateAccountComponent
-    },
-    {
-        path:'member-home',
-        component:MemberHomeComponent
-    },
-    { 
-    path:'account',
-    component:AccountComponent
-},
-{ 
-    path:'favorite-movie',
-    component:FavoriteMovieComponent
-},
-{ 
-    path:'my-review',
-    component:MyReviewComponent
-},
-{ 
-    path:'my-add-movie',
-    component:MyAddMovieComponent
-},
- 
-{ 
-    path:'movie-review/:movieIdPath',
-    component:MovieReviewComponent
-},
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: 'database', component: DatabaseComponent },
+  {
+    path: 'add-movie',
+    component: AddMovieComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'create-account',
+    component: CreateAccountComponent,
+  },
+  {
+    path: 'member-home',
+    component: MemberHomeComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+  },
+  {
+    path: 'favorite-movie',
+    component: FavoriteMovieComponent,
+  },
+  {
+    path: 'my-review',
+    component: MyReviewComponent,
+  },
+  {
+    path: 'my-add-movie',
+    component: MyAddMovieComponent,
+  },
 
-
+  {
+    path: 'movie-review/:movieIdPath',
+    component: MovieReviewComponent,
+  },
 ];

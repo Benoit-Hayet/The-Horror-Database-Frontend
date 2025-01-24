d6a45602df2f7ef93bec44a6589b69e50a6e16c8
Benoit-Hayet <hayetbenoit@gmail.com>
2025-01-24 15:14:52 +0100

    Admin review ok

--- src/services/review.service.ts
+++ src/services/review.service.ts
@@ -8,18 +8,32 @@ import { review } from '../app/model/review.model';
 export class ReviewService {
   private reviewUrl = 'http://localhost:8080/reviews';
 
-  constructor(private http: HttpClient, private authService: AuthService) {}
-  
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+  ) {}
 
   addReview(review: string, rating: number, movieId: number): Observable<any> {
-    const body = { review, rating, movie:{id: movieId} };
+    const body = { review, rating, movie: { id: movieId } };
     console.log('Objet envoyé:', body);
-  
+
     return this.http.post(this.reviewUrl, body).pipe(
       catchError((error) => {
-        console.error('Erreur lors de l\'envoi de la critique:', error);
+        console.error("Erreur lors de l'envoi de la critique:", error);
         return throwError(() => new Error('Erreur de requête'));
-      })
+      }),
+    );
+  }
+
+  getAllReviews(): Observable<review[]> {
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<review[]>(`${this.reviewUrl}`, { headers }).pipe(
+      catchError((error) => {
+        console.error('Erreur lors de la récupération des critiques:', error);
+        return throwError(() => new Error('Erreur de requête'));
+      }),
     );
   }
 
@@ -27,39 +41,44 @@ export class ReviewService {
     const token = this.authService.getToken();
     const id = this.authService.getUserId();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    
-    return this.http.get<review[]>(`${this.reviewUrl}/${id}`, { headers }).pipe(
 
+    return this.http.get<review[]>(`${this.reviewUrl}/${id}`, { headers }).pipe(
       catchError((error) => {
         console.error('Erreur lors de la récupération des critiques:', error);
         return throwError(() => new Error('Erreur de requête'));
-      })
+      }),
     );
   }
- 
-  updateReview(reviewId: number, updatedReview: { review: string; rating: number }): Observable<review> {
+
+  updateReview(
+    reviewId: number,
+    updatedReview: { review: string; rating: number },
+  ): Observable<review> {
     const token = this.authService.getToken();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     const body = {
       review: updatedReview.review,
-      rating: updatedReview.rating
+      rating: updatedReview.rating,
     };
-    console.log('Body : ',body);
-  
-    return this.http.patch<review>(`${this.reviewUrl}/${reviewId}`, body, { headers }).pipe(
-      catchError((error) => {
-        console.error('Erreur lors de la mise à jour de la critique :', error);
-        return throwError(() => new Error('Erreur lors de la mise à jour de la critique.'));
-      })
-    );
+    console.log('Body : ', body);
+
+    return this.http
+      .patch<review>(`${this.reviewUrl}/${reviewId}`, body, { headers })
+      .pipe(
+        catchError((error) => {
+          console.error(
+            'Erreur lors de la mise à jour de la critique :',
+            error,
+          );
+          return throwError(
+            () => new Error('Erreur lors de la mise à jour de la critique.'),
+          );
+        }),
+      );
   }
-  
-  
-  
+
   deleteReview(reviewId: number): Observable<any> {
     console.log('ID de la critique à supprimer:', reviewId);
     return this.http.delete(`${this.reviewUrl}/${reviewId}`);
   }
-  
-
 }

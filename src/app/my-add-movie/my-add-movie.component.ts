7c90d0a89c489ed8ce076ad9d1be1ceba4778bde
Benoit-Hayet <hayetbenoit@gmail.com>
2025-01-26 19:56:19 +0100

    Delete account

--- src/app/my-add-movie/my-add-movie.component.ts
+++ src/app/my-add-movie/my-add-movie.component.ts
@@ -1,32 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MemberNavbarComponent } from '../member-navbar/member-navbar.component';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { ApiService } from '../../services/api.service';
 import { AuthService } from '../../services/auth.service';
-import { Observable } from 'rxjs';
 import { ReviewService } from '../../services/review.service';
-import { review } from '../model/review.model';
 import { movieCards } from '../model/movieCards.model';
 import { MovieService } from '../../services/movie.service';
+import { AdminNavbarComponent } from '../admin-navbar/admin-navbar.component';
 
 @Component({
   selector: 'app-my-add-movie',
   standalone: true,
-  imports: [CommonModule, MemberNavbarComponent],
+  imports: [CommonModule, MemberNavbarComponent, AdminNavbarComponent],
   templateUrl: './my-add-movie.component.html',
   styleUrl: './my-add-movie.component.scss',
 })
 export class MyAddMovieComponent {
   addedMovieCards: movieCards[] = [];
   stars: number[] = [1, 2, 3, 4, 5];
+  isAdmin: boolean = false;
 
-  constructor(private reviewService: ReviewService, private authService: AuthService, private movieService: MovieService) {}
+  constructor(
+    private reviewService: ReviewService,
+    private authService: AuthService,
+    private movieService: MovieService,
+  ) {}
 
   ngOnInit() {
-    const decodedToken = this.authService.getDecodedToken(); // Récupérer le token décodé
+    const decodedToken = this.authService.getDecodedToken();
     if (decodedToken) {
-      const userId = decodedToken.id; // Utiliser l'ID de l'utilisateur décodé à partir du token
+      const userId = decodedToken.id;
+
+      this.isAdmin = decodedToken.roles.some(
+        (role: any) => role.authority === 'ROLE_ADMIN',
+      );
+
       this.movieService.getAddedMoviesByUserId().subscribe((response) => {
         console.log(response);
         this.addedMovieCards = response; // Assigner les films récupérés à la variable
--- src/app/my-review/my-review.component.ts
+++ src/app/my-review/my-review.component.ts
@@ -4,17 +4,19 @@ import { MemberNavbarComponent } from '../member-navbar/member-navbar.component'
 import { review } from '../model/review.model';
 import { ReviewService } from '../../services/review.service';
 import { AuthService } from '../../services/auth.service';
+import { AdminNavbarComponent } from '../admin-navbar/admin-navbar.component';
 
 @Component({
   selector: 'app-my-review',
   standalone: true,
-  imports: [CommonModule, MemberNavbarComponent],
+  imports: [CommonModule, MemberNavbarComponent, AdminNavbarComponent],
   templateUrl: './my-review.component.html',
   styleUrl: './my-review.component.scss',
 })
 export class MyReviewComponent {
   reviewMovieCards: review[] = [];
   stars: number[] = [1, 2, 3, 4, 5];
+  isAdmin: boolean = false;
 
   constructor(
     private reviewService: ReviewService,
@@ -26,44 +28,38 @@ export class MyReviewComponent {
     if (decodedToken) {
       const userId = decodedToken.id;
 
+      this.isAdmin = decodedToken.roles.some(
+        (role: any) => role.authority === 'ROLE_ADMIN',
+      );
+
       this.reviewService.getReviewsByUserId().subscribe((response) => {
         console.log(response); // Vérifiez si la réponse est bien reçue
         this.reviewMovieCards = response;
       });
     }
   }
-  /*updateReview(reviewId: number, updatedReview: { review: string; rating: number }): void {
-    this.reviewService.updateReview(reviewId, updatedReview).subscribe({
-      next: (response) => {
-        console.log('Critique mise à jour :', response);
-        // Mettre à jour les critiques affichées localement
-        const index = this.reviewMovieCards.findIndex((r) => r.id === reviewId);
-        if (index > -1) {
-          this.reviewMovieCards[index] = { ...this.reviewMovieCards[index], ...response };
-        }
-      },
-      error: (err) => {
-        console.error('Erreur lors de la mise à jour de la critique :', err);
-      }
-    });
-  }*/
 
-      updateReview(reviewId: number, updatedReview: { review: string; rating: number }): void {
+  updateReview(
+    reviewId: number,
+    updatedReview: { review: string; rating: number },
+  ): void {
     this.reviewService.updateReview(reviewId, updatedReview).subscribe({
       next: (response) => {
         console.log('Critique mise à jour :', response);
         // Mettre à jour les critiques affichées localement
         const index = this.reviewMovieCards.findIndex((r) => r.id === reviewId);
         if (index > -1) {
-          this.reviewMovieCards[index] = { ...this.reviewMovieCards[index], ...response };
+          this.reviewMovieCards[index] = {
+            ...this.reviewMovieCards[index],
+            ...response,
+          };
         }
       },
       error: (err) => {
         console.error('Erreur lors de la mise à jour de la critique :', err);
-      }
+      },
     });
   }
-  
 
   deleteReview(reviewId: number): void {
     this.reviewService.deleteReview(reviewId).subscribe({

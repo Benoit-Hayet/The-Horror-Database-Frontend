78176797b8d59d722b48e692da4ee53e0b35bc77
Benoit-Hayet <hayetbenoit@gmail.com>
2025-03-16 16:03:26 +0100

    service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -16,7 +16,12 @@ export class UserService {
   ) {}
 
   getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>('http://localhost:8080/users');
+    const token = this.authService.getToken();
+    console.log('Token récupéré:', token); // Centralisé dans AuthService
+    const id = this.authService.getUserId();
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<User[]>(`${this.apiUrl}`,{headers});
   }
 
   getToken(): string | null {
@@ -31,10 +36,8 @@ export class UserService {
     const id = this.authService.getUserId();
 
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    console.log('Headers envoyés:', headers);
     return this.http.get<User>(`${this.apiUrl}/${id}`, { headers }).pipe(
       catchError((error) => {
-        console.error('Error fetching user profile:', error);
         return throwError(() => error); // Utilisation de la syntaxe moderne
       }),
     );
